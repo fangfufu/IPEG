ae632efd6b45e54c1c6227be48624390e97033c6
Fufu Fang <fangfufu2003@gmail.com>
2017-02-14 15:03:05 +0000

    added the actual code

---
+++ plot_parametric_matlab.m
@@ -0,0 +1,20 @@
+function [h] = plot_parametric_matlab()
+%PLOT_MATLAB Plot a parametric function using Matlab
+h = fplot(@funcx, @funcy, [1 50*pi], 'MeshDensity', 100);
+axis equal;
+end
+
+function [p] = funcx(t)
+pa = func(t);
+p = pa(:,1:end/2);
+end
+
+function [p] = funcy(t)
+pa = func(t);
+p = pa(:,end/2+1:end);
+end
+
+function [p] = func(t)
+% Insert your Mathematica output here
+p = [0, 0];
+end
